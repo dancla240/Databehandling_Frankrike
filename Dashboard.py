520bd4f1ee9ba54730cda1bd280b7832fb854c68
Asajad Hussein <firstzpat@gmail.com>
2023-11-18 11:37:26 +0100

    Skapat en Dashboard.py

---
+++ Dashboard.py
@@ -0,0 +1,5 @@
+from dash import Dash, html, dcc, callback, Input, Output, dash_table
+import dash_bootstrap_components as dbc
+import plotly.express as px
+import pandas as pd
+from dash_bootstrap_templates import load_figure_template
