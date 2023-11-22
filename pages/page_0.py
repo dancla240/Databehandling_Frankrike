a53a7a2020125894a71277e360d02317aec6368f
Daniel Claesson <dacl@rocketmail.com>
2023-11-22 18:17:19 +0100

    Updated "Frågeställningar".

--- pages/page_0.py
+++ pages/page_0.py
@@ -32,7 +32,7 @@ layout = dbc.Container([
         html.P('2. Har Frankrike varit mest framgångsrikt i sommar eller vinter OS?'),
         html.P('3. Finns det någon tid i historien som kan sägas vara Frankrikes storhetstid i OS?'),
         html.P('4. Vilket var Frankrikes bästa OS?'),
-        html.P('5. Vilka sporter har Frankrike varit sämst i? Finns det någon sport de inte fått medalj i, tex?'),
+        html.P('5. Hur ser könsfördelningen ut i olika sportgrenar?'),
         html.P('6. Hur ser åldersfördelningen ut för Frankrike genom åren?'),
     ]),
     dbc.Row([
@@ -40,20 +40,11 @@ layout = dbc.Container([
     ]),
     dbc.Row([
         html.P('1. Titta igenom sporterna genom tiderna, vilka sporter har varit OS-sporter?'),
-        html.P('2. Finns det några gamla intressanta sporter att analysera? Eller sporter som vi är intresserade av?'),
+        html.P('2. Finns det några intressanta sporter att analysera?'),
         html.P('3. Dragkamp (Tug-Of-War): Medaljfördelning genom åren?'),
         html.P('4. Speed Skating: Medaljfördelning. Det finns mycket data för sporten.'),
         html.P('5. Tyngdlyftning: Ålder, vikt och längdfördelning över tid? Hade varit intressant att plotta resultaten över tid, men vi har inte den datan.'),
         html.P('6. Basket: Top 10 medaljfördelning alla OS.'),
         html.P('7. Brottning: Top 10 medaljfördelning alla OS.'),
-    ]),
-    dbc.Row([
-        html.H2('Antalet länder som har deltagit:'),
-        html.H3(athlete_events['NOC'].nunique())
-    ]),
-
-    dbc.Row([
-        html.H2('Länder:'),
-        html.H3(list(athlete_events['NOC'].unique())),
     ]),
 ], fluid=True)
